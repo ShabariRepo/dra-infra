4069fc71247aa9235652f29e4b59f2134fae6599
Shabari <appa@shabaris-mbp.lan>
2025-05-29 18:31:14 -0400

    - add scoped iam for gh runner

--- iam.tf
+++ iam.tf
@@ -17,3 +17,28 @@ resource "aws_iam_instance_profile" "runner_profile" {
   name = "github-actions-runner-profilex"
   role = aws_iam_role.runner_role.name
 }
+
+# Scoped policy for GH Runners
+resource "aws_iam_role_policy" "runner_inline_policy" {
+  name = "github-runner-policy"
+  role = aws_iam_role.runner_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "s3:PutObject",
+          "s3:GetObject"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
