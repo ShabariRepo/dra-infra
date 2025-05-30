c9e820132e416c7c37714e5b41127b756dfcc3b3
Shabari <appa@shabaris-mbp.lan>
2025-05-29 20:12:28 -0400

    - add kubeconfig for EKS

--- eks.tf
+++ eks.tf
@@ -42,3 +42,24 @@ resource "aws_eks_node_group" "this" {
     Name = "deepr-eks-nodes"
   }
 }
+
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = aws_iam_role.eks_node_role.arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      }
+    ])
+  }
+}
--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = var.aws_region #"us-east-1" # or whichever region you're using
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
